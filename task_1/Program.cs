251b7abfc144c819ffe9556fe4cf216ef5cf234b
Pavel Kulik <Kanstantin0105@gmail.com>
2023-04-25 17:49:38 +0300

    first task ready

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите первое число: ");
+        int firstNamber = int.Parse(Console.ReadLine()!);
+        Console.Write("Введите второе число: ");
+        int secondNamber = int.Parse(Console.ReadLine()!);
+        Console.Write("a = " + firstNamber + "; b = " + secondNamber + " -> max = ");
+        if (firstNamber > secondNamber) Console.WriteLine(firstNamber);
+        else Console.WriteLine(secondNamber);
+    }
+}
