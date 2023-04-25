89f4a859c5b72b6e0d91111a38070e03751b7d13
Pavel Kulik <Kanstantin0105@gmail.com>
2023-04-25 18:28:22 +0300

    third task ready

---
+++ task_3/Program_3.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine()!);
+        Console.Write(number + " -> ");
+
+        if (number % 2 == 0) Console.WriteLine("да");
+        else Console.WriteLine("нет");
+    }
+}
