072c7ff64d3acd786dd48c1a8ca79817cb6e2046
Pavel Kulik <Kanstantin0105@gmail.com>
2023-04-25 18:19:45 +0300

    second task ready

---
+++ task_2/Program_2.cs
@@ -0,0 +1,24 @@
+﻿internal partial class Program_2
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите первое число: ");
+        int firstNumber = int.Parse(Console.ReadLine()!);
+        Console.Write("Введите второе число: ");
+        int secondNumber = int.Parse(Console.ReadLine()!);
+        Console.Write("Введите третье число: ");
+        int thirdNumber = int.Parse(Console.ReadLine()!);
+        
+        Console.Write(firstNumber + ", " + secondNumber + ", " + thirdNumber + " -> ");
+        
+        if (firstNumber > secondNumber && firstNumber > thirdNumber)
+        {
+            Console.WriteLine(firstNumber);
+        }
+        else if (secondNumber > firstNumber && secondNumber > thirdNumber)
+        {
+            Console.WriteLine(secondNumber);
+        }
+        else Console.WriteLine(thirdNumber);
+    }
+}
