89625fcbfc5930db9098682ab582ea25f59c0b2e
Pavel Kulik <Kanstantin0105@gmail.com>
2023-04-25 18:39:59 +0300

    fourth task ready

---
+++ task_4/Program_4.cs
@@ -0,0 +1,13 @@
+﻿internal partial class Program_4
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine()!);
+        Console.Write(number + " -> ");
+
+        for (int i = 2; i <= number; i = i + 2) Console.Write(i + " ");
+
+        Console.WriteLine();
+    }
+}
