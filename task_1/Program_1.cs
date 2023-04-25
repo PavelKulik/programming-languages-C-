affcf46ef133ea5d26a6ca24de13e02bd266bcef
Pavel Kulik <Kanstantin0105@gmail.com>
2023-04-25 18:18:41 +0300

    rename file

---
+++ task_1/Program_1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+internal class Program_1
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите первое число: ");
+        int firstNamber = int.Parse(Console.ReadLine()!);
+        Console.Write("Введите второе число: ");
+        int secondNamber = int.Parse(Console.ReadLine()!);
+        Console.Write("a = " + firstNamber + "; b = " + secondNamber + " -> max = ");
+        if (firstNamber > secondNamber) Console.WriteLine(firstNamber);
+        else Console.WriteLine(secondNamber);
+    }
+}
